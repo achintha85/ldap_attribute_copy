b1d8ce8bdf95522b296e8b7f977b825630937ae4
Archie Gunasekara <achintha@me.com>
2015-09-02 11:24:09 +1000

    Better way to configure the script. Configuration section was added.

--- ldap_edit.php
+++ ldap_edit.php
@@ -1,13 +1,23 @@
 <?php
 
 /**
- * This script gets a copy of users with a attribute called new_mail
- * Then copies the new_mail attribute value to mail attribte
- * You'll be able to modiy it to perform similar functions
+ * This script gets a copy of users with a attribute conifugred in the configuration called ATTRIBUTE_1
+ * Then copies the ATTRIBUTE_1 attribute value to configured attribute called ATTRIBUTE_2
+ * Please modify the configuration below to meet your needs
  */
+ 
+/**********************
+ * Script Configuration
+ **********************/ 
 $ldap_password = 'PASSWORD';
 $ldap_username = 'cn=USER,dc=DC1,dc=DC2,dc=DC3';
-$ldap_connection = ldap_connect('HOSTNAME');
+$ldap_host = 'HOSTNAME';
+$ldap_base_dn = 'ou=users,dc=DC1,dc=DC2,dc=DC3';
+$attribute_to_copy_from = 'ATTRIBUTE_1';
+$attribute_to_copy_to = 'ATTRIBUTE_2';
+/**********************/
+
+$ldap_connection = ldap_connect('$ldap_host');
 
 if (FALSE === $ldap_connection) {
 
@@ -20,13 +30,12 @@ ldap_set_option($ldap_connection, LDAP_OPT_REFERRALS, 0); // We need this for do
 
 if (TRUE === ldap_bind($ldap_connection, $ldap_username, $ldap_password)){
 
-	$ldap_base_dn = 'ou=users,dc=DC1,dc=DC2,dc=DC3';
 	$search_filter = '(&)';
 	$attributes = array();
 	$attributes[] = "uid";
 	$attributes[] = "displayname";
-	$attributes[] = "mail";
-	$attributes[] = "new_mail";
+	$attributes[] = $attribute_to_copy_to;
+	$attributes[] = $attribute_to_copy_from;
 	$result = ldap_search($ldap_connection, $ldap_base_dn, $search_filter, $attributes);
 
 	if(FALSE !== $result) {
@@ -35,20 +44,20 @@ if (TRUE === ldap_bind($ldap_connection, $ldap_username, $ldap_password)){
 
 		for($x=0; $x<$entries['count']; $x++) {
 	      
-			if(!empty($entries[$x]['new_mail'][0])) {
+			if(!empty($entries[$x][$attribute_to_copy_from][0])) {
 				
 				$entry = array();
 		
 				$dn = strtolower(trim($entries[$x]['dn']));
-				$new_email = strtolower(trim($entries[$x]['new_mail'][0]));
-				$entry['mail'][] = $new_email;
+				$copy_from_value = strtolower(trim($entries[$x][$attribute_to_copy_from][0]));
+				$entry[$attribute_to_copy_to][] = $copy_from_value;
 				$result = ldap_mod_replace($ldap_connection, $dn, $entry);	
 
 				$ad_users[] = array(
 					'uid' => strtolower(trim($entries[$x]['uid'][0])),
 					'displayname' => strtolower(trim($entries[$x]['displayname'][0])),
-					'mail' => strtolower(trim($entries[$x]['mail'][0])),
-					'new_email' => $new_email,
+					$attribute_to_copy_to => strtolower(trim($entries[$x][$attribute_to_copy_to][0])),
+					$attribute_to_copy_from => $copy_from_value,
 					'dn' => $dn,
 					'successfully_modified' => $result
 				);
@@ -63,4 +72,4 @@ print_r($ad_users);
 echo "\n\n";
 echo "Retrieved ". count($ad_users) ." Active Directory users\n";
 
-?> 
+?>
